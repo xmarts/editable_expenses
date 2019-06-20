a0364baa24df04d10a36e3b17243fb226565d5df
luis <angelchicles97@gmail.com>
2019-06-20 12:52:20 -0500

    modificacion

--- editable_expenses/models/models.py
+++ editable_expenses/models/models.py
@@ -7,5 +7,13 @@ class ranplace_campo(models.Model):
 
     expense_line_ids = fields.One2many('hr.expense', 'sheet_id', string='Expense Lines')
     
+class remplace(models.Model):
+    _inherit ='hr.expense'
+                
+    product_id = fields.Many2one('product.product', string='Product')
+    unit_amount = fields.Float(string='Unit Price', )
+    quantity = fields.Float(string="Cantidad")
+
+    
 
         
