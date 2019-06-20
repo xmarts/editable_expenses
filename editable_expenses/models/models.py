bdf6e2759f17344d0eeea0f8a9f96df699ff9138
luis <angelchicles97@gmail.com>
2019-06-20 13:45:59 -0500

    states

--- editable_expenses/models/models.py
+++ editable_expenses/models/models.py
@@ -10,9 +10,9 @@ class ranplace_campo(models.Model):
 class remplace(models.Model):
     _inherit ='hr.expense'
                 
-    product_id = fields.Many2one('product.product', string='Product')
-    unit_amount = fields.Float(string='Unit Price', )
-    quantity = fields.Float(string="Cantidad")
+    product_id = fields.Many2one('product.product', string='Product',states={'draft': [('readonly', False)], 'refused': [('readonly', False)],'approced': [('readonly', False)]})
+    unit_amount = fields.Float(string='Unit Price',states={'draft': [('readonly', False)], 'refused': [('readonly', False)],'approced': [('readonly', False)]} )
+    quantity = fields.Float(string="Cantidad",states={'draft': [('readonly', False)], 'refused': [('readonly', False)],'approced': [('readonly', False)]})
 
     
 
